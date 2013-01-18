6cff43c8ea0bb62f8dd7a7023d3e6dc4b238ed62
liligeng111 <liligeng111@gmail.com>
2013-01-19 00:18:19 +0800

    revised

--- map.js
+++ map.js
@@ -7,15 +7,20 @@ var min_date;
 var max_date;
 var low_bound;
 var high_bound;
+var middle_point;
+var changed;
+var circles;
 
 function initialize() {
     cdate = new Date(2004,6,1);
     min_date = -1;
     max_date= 99999999;
-    repaint();
+    changed = true;
     //update_layer();
-    //time_start();
-
+    time_start();
+    analysis();
+    changed = true;
+    circles = [];
 }
 
 function repaint()
@@ -67,6 +72,9 @@ function analysis()
     {
 	min_date = (line[1].split(','))[2];
 	max_date = (line[line.length - 2].split(','))[2];
+      low_bound = calDate(0);
+      high_bound = calDate(1000); 
+      middle_point = calDate(500);
     }
     for (var i = 1; i < line.length; i++)
     {
@@ -76,7 +84,7 @@ function analysis()
 	}
 	var found = false;
 	var temp = line[i].split(',');
-	var temp_date = parseInt(temp[2]);
+	var temp_date = number_to_date(parseInt(temp[2]));
 	if (temp_date < low_bound)
 	{
 		continue;
@@ -90,13 +98,14 @@ function analysis()
 		if(temp[0] == places[j].name)
 		{
 			places[j].count += parseInt(temp[1]);
+			places[j].rate += parseInt(temp[1]) * (number_to_date(temp[2]).getTime() - middle_point.getTime());
 			found = true;
 			break;
 		}
 	}
 	if (!found)
 	{
-		var p = {name:temp[0],count:parseInt(temp[1]),lng:temp[3],lat:temp[4]};
+		var p = {name:temp[0],count:parseInt(temp[1]),lng:temp[3],lat:temp[4],rate: parseInt(temp[1]) * (number_to_date(temp[2]).getTime() - middle_point.getTime())};
 		places.push(p);
 	}
 
@@ -106,20 +115,47 @@ function analysis()
 function layer()
 {
     var cityCircle;
+    var total = 0;    
     for (var i = 0; i < places.length; i++)
     {
+        total += places[i].count;
+        places[i].rate /= places[i].count * (high_bound.getTime() - low_bound.getTime());
+        //console.log(places[i]);
+    }
+
+    for (var i = 0; i < places.length; i++)
+    {
+       //console.log(places[i].name + ": " + rgb(places[i].rate));
         var populationOptions = {
-          strokeColor: "#FF0000",
-          strokeOpacity: 0.8,
+          strokeColor: rgb(places[i].rate),
+          strokeOpacity: 0.0,
           strokeWeight: 2,
-          fillColor: "#FF0000",
+          fillColor: rgb(places[i].rate),
           fillOpacity: 0.35,
           map: map,
           center: new google.maps.LatLng(places[i].lat, places[i].lng),
-          radius: places[i].count * 1000
+          radius: places[i].count * 5000000 / total
         };
         cityCircle = new google.maps.Circle(populationOptions);
+	  circles[places[i].name] = cityCircle;
     }
+}
+
+function rgb(rate)
+{
+var r = 128 + 256 * rate;
+var b = 128 + -256 * rate;
+var g = 128
+return '#' + prune(r) + prune(g) + prune(b);
+}
+
+function prune(v)
+{
+   if (v < 0) v = 0;
+   if (v > 255) v = 255;
+   var t = parseInt(v).toString(16);
+   if (t.length ==1) t = '0' + t;
+   return t;
 }
 
 function update_layer() {
@@ -159,21 +195,32 @@ function number_to_date(mynumber) {
 
 function time_start() {
     isstart = true;
-    //datetimer = setTimeout("update_date()",1000);
+    datetimer = setTimeout("update_date()",1000);
 }
 
 function update_date() {
-    cdate.setDate(cdate.getDate() + 1);
-    document.getElementById("map_date").innerHTML=cdate.toDateString();
-    update_layer();
+    //cdate.setDate(cdate.getDate() + 1);
+    //document.getElementById("map_date").innerHTML=cdate.toDateString();
+    if (changed == true)
+    {
+	  	changed = false;
+		repaint();
+    }
     if (isstart) {
         datetimer = setTimeout("update_date()", 1000);
     }
 }
 
+function UTC(date)
+{	
+	return Date.UTC(date.substring(0,4),date.substr(4,2),date.substr(6,2));
+}
+
 function calDate(date)
 {
-	return parseInt(min_date) + parseInt((max_date - min_date) * date / 1000);	
+var utc = UTC(min_date) + (UTC(max_date) - UTC(min_date)) * date / 1000;	
+//console.log(new Date(utc));
+return new Date(utc);
 }
 
   $(function() {
@@ -182,16 +229,17 @@ function calDate(date)
       range: true,
       min: 0,
       max: 1000,
-      values: [ 75, 300 ],
+      values: [0, 1000],
       slide: function( event, ui ) {
-        $( "#amount" ).val(calDate(ui.values[0]) + " - " + calDate(ui.values[1]) );
+        $( "#amount" ).val(calDate(ui.values[0]).toDateString() + " - " + calDate(ui.values[1]).toDateString() );
+
 		low_bound = calDate(ui.values[0]);
 		high_bound = calDate(ui.values[1]);
-		repaint();
+		middle_point = calDate((ui.values[0] + ui.values[1]) / 2);
+		changed = true;
       }
     });
-    $( "#amount" ).val( calDate($( "#slider-range" ).slider( "values", 0 )) +
-      " - " + calDate($( "#slider-range" ).slider( "values", 1 )) );
-		repaint();
+    $( "#amount" ).val('Select Date:');
+		changed = true;
   });
 
