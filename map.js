517ef95abb0b238de2ab8abd652539dcb2ba8ac9
Dai Zuozhuo <daizuozhuo@gmail.com>
2013-01-19 20:15:56 -0800

    不想写了

--- map.js
+++ map.js
@@ -134,11 +134,10 @@ function layer()
           fillOpacity: 0.35,
           map: map,
           center: new google.maps.LatLng(places[i].lat, places[i].lng),
-          radius: places[i].count * 5000000 / total
+          radius: Math.log(places[i].count) * 1000000000 / total
         };
         cityCircle = new google.maps.Circle(populationOptions);
 	  circles[places[i].name] = cityCircle;
-    }
 }
 
 function rgb(rate)
